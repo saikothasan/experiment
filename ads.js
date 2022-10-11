370f70b20bc6282ec0396e1a47318210c64a51f7
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2022-10-11 22:51:26 +0600

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,1 @@
+!function(){var a=document.createElement("script"),b=document.createAttribute("data-ad-client");b.value="ca-pub-5102551212471159",a.setAttributeNode(b),a.defer=!0,a.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";var c=document.getElementsByTagName("head")[0];c.parentNode.insertBefore(a,c)}();
